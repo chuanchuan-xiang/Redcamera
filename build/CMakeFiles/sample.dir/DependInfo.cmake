970ae6431f1a957b3fb6388197e620dff11f4166
ccx <1750693215@qq.com>
2025-10-28 16:51:03 +0800

    第一次

---
+++ build/CMakeFiles/sample.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ccx/Redcamera/camera.cpp" "/home/ccx/Redcamera/build/CMakeFiles/sample.dir/camera.cpp.o"
+  "/home/ccx/Redcamera/cmd.cpp" "/home/ccx/Redcamera/build/CMakeFiles/sample.dir/cmd.cpp.o"
+  "/home/ccx/Redcamera/data.cpp" "/home/ccx/Redcamera/build/CMakeFiles/sample.dir/data.cpp.o"
+  "/home/ccx/Redcamera/display.cpp" "/home/ccx/Redcamera/build/CMakeFiles/sample.dir/display.cpp.o"
+  "/home/ccx/Redcamera/sample.cpp" "/home/ccx/Redcamera/build/CMakeFiles/sample.dir/sample.cpp.o"
+  "/home/ccx/Redcamera/temperature.cpp" "/home/ccx/Redcamera/build/CMakeFiles/sample.dir/temperature.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
